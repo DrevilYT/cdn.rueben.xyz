521e262f806224195a583721ef52bb59402f8c3f
DrevilYT <68735288+DrevilYT@users.noreply.github.com>
2026-01-05 18:15:39 +0800

    Add files via upload

---
+++ content/unlockercli/patchercli-beta/1966720-lc.lua
@@ -0,0 +1,12 @@
+-- Lethal​​‌‌‌​​​​​‌‌​​‌​​​‌‌​‌‌‌​​‌‌‌​​‌​‌‌​​‌‌​​​‌‌‌​​​​‌‌​​​‌​​​‌‌​‌​​​‌‌​​‌‌​​‌‌​​​‌‌​‌‌​​‌​​​‌‌​​‌‌​​​‌‌​​​‌​​‌‌‌​​​​​‌‌​‌​‌​​‌‌‌​​‌ Company
+-- AppID 1966720 | Generated on 2026-01-05 09:58 UTC | openlua.cloud
+-- Note: This game supports Steam Workshop content
+
+-- Main Application
+addappid(1966720, 1, "b1b18f9400b7b45bb7d3223ca7951a73cc2bdf0cb311e5b1a337e9c776c7ac7d")
+
+-- Content Depots (1)
+addappid(1966721, 1, "b79233b678dae6d3f928ff9c6c7674d559427776a1337dee238773a4fec60949") -- Lethal Company - Content
+
+-- Shared Depots (1)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (from 228980)
