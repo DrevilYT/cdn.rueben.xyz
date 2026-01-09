dba8ccd9d92234349500013832814380fb6ae625
DrevilYT <68735288+DrevilYT@users.noreply.github.com>
2026-01-09 16:04:25 +0800

    Update Repo patcher for Rueben Software.bat

--- content/unlockercli/patchercli-beta/Repo patcher for Rueben Software.bat
+++ content/unlockercli/patchercli-beta/Repo patcher for Rueben Software.bat
@@ -13,10 +13,12 @@ mkdir REPO
 cd REPO
 curl -o online.rar "https://cdn.rueben.xyz/content/unlockercli/patchercli-beta/repo-online.rar"
 tar -xf online.rar
+del online.rar
 cls
 echo Injecting into Steam and cleaning up . . . 
 timeout /t 3 >nul
 powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Rueben Software', 'Steam successfully hijacked and install mod returned success with error level 0', [System.Windows.Forms.ToolTipIcon]::None)}"
 cd C:\Program Files (x86)\Steam
 start steam.exe
+
 exit
